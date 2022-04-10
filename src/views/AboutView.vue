8da6717f7ba0c6db18f0434d85e3a3359ce97cda
trexcy-chan <trexcyfe.torrefiel@lsu.edu.ph>
2022-04-10 12:01:54 +0800

    first

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>  
+<!-- component -->
+<Datepicker v-model="date"></Datepicker>
+</template>
+
+<script>
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+export default {
+ components: { Datepicker },
+        data() {
+            return {
+                date: null,
+            };
+        }
+
+}
+</script>
---
+++ src/views/AddResident.vue
@@ -0,0 +1,13 @@
+<template>
+ <Datepicker v-model="date" range />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
